49fec171241e9c36134401b5e1ebd973cfe5ec80
markdenardo <mark.denardo@gmail.com>
2021-08-20 15:18:54 -0400

    nothing

--- loops.lua
+++ loops.lua
@@ -1,10 +1,9 @@
-
 --dice roll game
 print("Hello")
 
 local dice1 = game.Workspace.Game.Dice_Parts.dice_1
 local dice2 = game.Workspace.Game.Dice_Parts.dice_2
-local randRot = math.random(1,10)
+local randRot = math.random(1, 10)
 local randPos = math.random(-11, 11)
 
 --dice.dice_1.Position = Vector3.new(-11, 150, 8)
@@ -16,8 +15,8 @@ dice2.BrickColor = BrickColor.new("Bright blue")
 dice1.Position = Vector3.new(randPos, 150, randPos)
 dice2.Position = Vector3.new(randPos, 120.578, randPos)
 
-dice1.RotVelocity = Vector3.new(randRot,randRot,randRot)
-dice2.RotVelocity = Vector3.new(randRot,randRot,randRot)
+dice1.RotVelocity = Vector3.new(randRot, randRot, randRot)
+dice2.RotVelocity = Vector3.new(randRot, randRot, randRot)
 
 dice1.Anchored = false
 wait(0.5)
@@ -30,9 +29,8 @@ local clickDetector = game.Workspace.Game.ClickDetector
 
 part.BrickColor = BrickColor.new("Alder")
 
-
 function diceRoll(part)
-	part.Position = Vector3.new(randPos,randPos,randPos)
+	part.Position = Vector3.new(randPos, randPos, randPos)
 end
 
 function onMouseClick()
@@ -40,7 +38,6 @@ function onMouseClick()
 end
 
 clickDetector.MouseClick:connect(onMouseClick)
-
 
 --local clickDetector = game.Workspace.Game.ClickDetector
 
@@ -50,18 +47,18 @@ clickDetector.MouseClick:connect(onMouseClick)
 
 --clickDetector.MouseClick:connect(onMouseClick)
 
-
 -- loops
 
 local text = "I love Roblox"
 
-for i=1,10 do print(text) end
+for i = 1, 10 do
+	print(text)
+end
 
 --
 
-
 --for i=1,20 do Instance.new("Part", game.Workspace)
-		
+
 --	game.Workspace.Part.Anchored = true
 --	game.Workspace.Part.Position = Vector3.new(randPos,randPos,randPos)
 --	print('part',i)
@@ -74,7 +71,6 @@ for i=1,10 do print(text) end
 --	randomPart.Parent = game.Workspace
 --	return randomPart
 --end
-
 
 --for i=1, 20 do makePart()
 --end
@@ -93,20 +89,18 @@ for i=1,10 do print(text) end
 --	i=i+1
 --end
 
-	
-
 function generatePart()
 	local randomPart = Instance.new("Part")
-	local newRandPos = math.random(-10,10)
+	local newRandPos = math.random(-100, 100)
 	randomPart.Anchored = false
-	randomPart.Position = Vector3.new(newRandPos,newRandPos, newRandPos)
+	randomPart.Position = Vector3.new(newRandPos, newRandPos, newRandPos)
 	randomPart.Parent = game.Workspace
 end
 
 generatePart()
 
-for i=0, 100 do generatePart()
+for i = 0, 100 do
+	generatePart()
 	wait(0.5)
-	print('part',i)
+	print("part", i)
 end
-	
